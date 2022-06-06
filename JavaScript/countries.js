dcc5ebcb5e92ffdafa89c9182fcdf588b3e88bb7
KsushaMironovich <mironovich.ksu@gmail.com>
2022-06-06 14:53:45 +0300

    ok

---
+++ JavaScript/countries.js
@@ -0,0 +1,8 @@
+<scritp>
+
+<input type='button' value='приветствие' onclick='alert("Добрый день!")'></input>
+<input type='button' value='приветствие' onclick='alert("Добрый день!")'></input>
+<input type='button' value='приветствие' onclick='alert("Добрый день!")'></input>
+<input type='button' value='приветствие' onclick='alert("Добрый день!")'></input>
+
+</scritp>
